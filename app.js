6690cdbf4347c46e7e26442bc1ee458eaba76b01
CrazyBotBoy <57533877+PasinduDushan@users.noreply.github.com>
2022-02-19 23:45:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,75 +5,29 @@ app.use(express.json());
 
 app.post("/command_1", async (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
-    res.sendStatus(400);
-    return;
-  }
-
-  res.send({ userId: 0 });
+  
+  //Write your code
 });
 
 app.post("/command_2", async (req, res) => {
-  const AuthToken = req.headers.authorization;
+  // example of how to get authentication token. => const AuthToken = req.headers.authorization;
   const { username, password } = req.body;
 
-  if (
-    !AuthToken ||
-    !username ||
-    !password ||
-    AuthToken !== "testing_token_1234" ||
-    username !== "username" ||
-    password !== "password"
-  ) {
-    res.sendStatus(401);
-    return;
-  }
-
-  res.sendStatus(200);
+  //Write your code
 });
 
 app.post("/command_3", async (req, res) => {
-  const AuthToken = req.headers.authorization;
+  //Example of how to get authentication token => const AuthToken = req.headers.authorization;
   const { username, password } = req.body;
 
-  if (
-    !AuthToken ||
-    !username ||
-    !password ||
-    AuthToken !== "testing_token_1234" ||
-    username !== "username" ||
-    password !== "password"
-  ) {
-    res.sendStatus(401);
-    return;
-  }
-
-  res.send({ userId: 12345678 });
+  //Write your code
 });
 
 app.post("/command_4", async (req, res) => {
-  const AuthToken = req.headers.authorization;
+  //Example of how to get authentication token => const AuthToken = req.headers.authorization;
   const { username, password, base64code } = req.body;
 
-  var base64 =
-    /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/;
-
-  const encoded_string = base64.test(base64code);
-
-  if (
-    !AuthToken ||
-    !username ||
-    !password ||
-    AuthToken !== "testing_token_1234" ||
-    username !== "username" ||
-    password !== "password" ||
-    encoded_string === false
-  ) {
-    res.sendStatus(401);
-    return;
-  }
-
-  res.send({ userId: 12345678 });
+  //Write your code
 });
 
 module.exports = app;
